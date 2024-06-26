f2713d6a9d2c73dd06f5caba881073828d3a5bb8
sarahrajabzadeh <khode.divergent@gmail.com>
2024-06-26 18:43:24 +0200

    update cmnts

--- data.py
+++ data.py
@@ -1,8 +1,11 @@
 import numpy as np
 import sys
 
+# Get the number of data points from the command line arguments
 size = int(sys.argv[1])
-data = np.random.rand(size, 2) * 100  # Generate random data
+
+# Generate random data
+data = np.random.rand(size, 2) * 100  
 
 # Save to CSV
 np.savetxt('dataset.csv', data, delimiter=',')
